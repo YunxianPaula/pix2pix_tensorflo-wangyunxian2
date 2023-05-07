dbfa1583297ab5cfce5adf73f1c471d9c0d38688
YunxianPaula <ywang090@gold.ac.uk>
2023-05-07 23:42:25 +0100

    add

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const SIZE = 256, sampleNum = 7;
-let inputCanvas, outputContainer, statusMsg, transferBtn, sampleIndex = 0, modelReady = false, isTransfering = false;
+let inputCanvas, outputContainer, statusMsg,textElement, transferBtn, sampleIndex = 0, modelReady = false, isTransfering = false;
 const inputImgs = [], outputImgs = [];
 
 const edges2pikachu = pix2pix('./models/photos_BtoA11.pict', modelLoaded);
@@ -12,6 +12,7 @@ function setup() {
   // Selcect output div container
   outputContainer = select('#output');
   statusMsg = select('#status');
+  textElement = select('#textElementp');
   transferBtn = select('#transferBtn').hide();
 
   // Display initial input image
@@ -29,6 +30,7 @@ function setup() {
       let outImg = createImg(`./images/output${i}.png`);
       outImg.hide().class('border-box');
       outputImgs.push(outImg);
+     
     });
   }
 
@@ -47,6 +49,7 @@ function draw() {
 function mouseReleased() {
   if (modelReady && !isTransfering) {
     transfer()
+
   }
 }
 
@@ -63,8 +66,17 @@ function transfer() {
     outputContainer.html('');
     // Create an image based result
     createImg(result.src).class('border-box').parent('output');
-    statusMsg.html('Done!');
+    statusMsg.html('done!');
+     // Create a new paragraph element and append it to the output container
+    //  let textElement = createP(' Zhou Gong interprets dreams:1. To dream of bamboo is a symbol of great wealth, implying that if you meet a son, you will become a powerful person in the future. 2. Dreaming that the yard is full of bamboo is a very auspicious dream, a dream that brings prosperity to the family and guarantees the future of future generations.3. Dreaming that the yard is full of bamboo, you will have good luck and everything goes well. Entrepreneurs dream that the yard is full of bamboo, and their career will be improved to a higher level.4. An office worker dreams that the yard is full of bamboo, indicating that you may be promoted and raise your salary.5. Dreaming of entering the bamboo forest and being unable to get out is an ominous sign, implying that disasters will continue in the near future, so you must be cautious in your affairs.6. To dream of towering bamboo is a bad omen, implying that you will offend someone because you speak too straight, and the other party may retaliate against you. Pay special attention to your words and deeds, and leave room for what you say.Psychological dream interpretation 1. Dream interpretation: One of the characteristics of bamboo is that it can be bent. It shows obedience and accommodation, and at the same time it symbolizes the strength of endurance, thus reflecting the same character. 2. Psychoanalysis: Bamboo represents good education, health, longevity and a fulfilling life. It shows due forbearance in difficult situations that call for an intelligent response. 3. Spiritual symbol: Bamboo symbolizes a perfect person who is good at seeking perfection. If you realize that you have these two characters of bamboo, you can calmly deal with the various breaks in your character.');
+    //  textElement.parent('outputContainer');
+   
+    textElement.html('<h1>Zhou Gong interprets dreams:</h1> <p>1. To dream of bamboo is a symbol of great wealth, implying that if you meet a son, you will become a powerful person in the future.</p> <p>2. Dreaming that the yard is full of bamboo is a very auspicious dream, a dream that brings prosperity to the family and guarantees the future of future generations.</p><p>3. Dreaming that the yard is full of bamboo, you will have good luck and everything goes well. Entrepreneurs dream that the yard is full of bamboo, and their career will be improved to a higher level.</p> <p>4. An office worker dreams that the yard is full of bamboo, indicating that you may be promoted and raise your salary.</p><h2> Psychological dream interpretation</h2> <p>1. Dream interpretation: One of the characteristics of bamboo is that it can be bent. It shows obedience and accommodation, and at the same time it symbolizes the strength of endurance, thus reflecting the same character.</p> <p>2. Psychoanalysis: Bamboo represents good education, health, longevity and a fulfilling life. It shows due forbearance in difficult situations that call for an intelligent response. </p> <p>3. Spiritual symbol: Bamboo symbolizes a perfect person who is good at seeking perfection. If you realize that you have these two characters of bamboo, you can calmly deal with the various breaks in your character.</p></div>');
+  
+    
+
     isTransfering = false;
+
   });
 }
 
